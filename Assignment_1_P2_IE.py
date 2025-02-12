92dfea6844161154842d8af558f7706e9805387d
imchaves1982 <irene.east@csuglobal.edu>
2025-02-12 04:07:20 -0700

    Add files via upload
    Assignment 1 Uploads

---
+++ Assignment_1_P2_IE.py
@@ -0,0 +1,13 @@
+#Critical Thinking Assignment 1 
+#Irene East
+#Part 2:
+
+#Ask user for 2 numbers
+num1 = int(input('Enter a number between 1 to 100:'))
+num2 = int(input('Enter a second number between 1 to 100:'))
+
+#Add numbers given by user
+print(num1,'*',num2,'=',num1*num2)
+
+#Subtract numbers given by user
+print(num1,'/',num2,'=',num1/num2)
